06f716c49efb580cf456b97853719463bc59ad2b
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-01-03 20:15:05 +0300

    add reports page

--- build/assets/js/dark-mode-toggle.js
+++ build/assets/js/dark-mode-toggle.js
@@ -1,5 +1,14 @@
 const toggle = document.getElementById("dark-mode");
 
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark');
+  toggle.checked = true;
+} else {
+  document.documentElement.classList.remove('dark')
+  toggle.checked = false;
+}
+
 toggle.addEventListener("change", () => {
   document.documentElement.classList.toggle("dark", toggle.checked);
+  localStorage.setItem('color-theme', toggle.checked ? 'dark' : 'light');
 });
